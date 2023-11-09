b07a6441fc7a3784f24308200d4cfd81a760076f
Mohammed Muzammil <122904557+mohdmuzzu@users.noreply.github.com>
2023-11-09 19:08:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import HeroSection from "./Components/HeroSection";
+import Card from "./Components/Card";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Card />
+    </>
+  );
 }
 
 export default App;
