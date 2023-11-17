cd3c2f0acf95119b44e451c7e846a638ad9d9c99
Mohammed Muzammil <122904557+mohdmuzzu@users.noreply.github.com>
2023-11-17 20:24:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import HeroSection from "./Components/HeroSection";
-import Card from "./Components/Card";
+import CardSection from "./Components/CardSection/CardSection";
+//import Card from "./Components/Card";
+import { useEffect } from "react";
+
+const ENDPOINT = "https://qtify-backend-labs.crio.do/";
 
 function App() {
+  const [topAlbums, setTopAlbums] = useState([]);
+  const [newAlbums, setNewAlbums] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${ENDPOINT}albums/top`).then(({ data }) => {
+      setTopAlbums(data);
+    });
+    axios.get(`${ENDPOINT}albums/new`).then(({ data }) => {
+      setNewAlbums(data);
+    });
+  }, []);
   return (
     <>
       <Navbar />
       <HeroSection />
-      <Card />
+      <CardSection tittle="Top Albums" data={topAlbums} />
+      <CardSection tittle="New Albums" data={newAlbums} />
     </>
   );
 }
