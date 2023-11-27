43c0e999263896ab3019b7ee9d1677d101d9c256
Mohammed Muzammil <122904557+mohdmuzzu@users.noreply.github.com>
2023-11-27 21:23:47 +0530

    Add files via upload

---
+++ FilterSection/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+import Carousel from "../Carousel";
+
+export default (tittle, data) => {
+  return (
+    <div className="section">
+      <div>
+        <h1 className="tittle">{tittle}</h1>
+      </div>
+      <Carousel data={data} />
+    </div>
+  );
+};
