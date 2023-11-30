2735125bbb7d80fd6e2f37a56a1ef387c79f51bd
Mohammed Muzammil <122904557+mohdmuzzu@users.noreply.github.com>
2023-11-30 21:40:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,16 +15,22 @@ function App() {
   const [topAlbums, setTopAlbums] = useState([]);
   const [newAlbums, setNewAlbums] = useState([]);
   const [Songs, setSongs] = useState([]);
+  const [filteredSongs, setfilteredSongs] = useState([]);
+  const [genres, setGenre] = useState([]);
 
   useEffect(() => {
-    axios.get(`${ENDPOINT}albums/top`).then(({ data }) => {
-      setTopAlbums(data);
+    axios.get(`${ENDPOINT}albums/top`).then((res) => {
+      setTopAlbums(res.data);
     });
-    axios.get(`${ENDPOINT}albums/new`).then(({ data }) => {
-      setNewAlbums(data);
+    axios.get(`${ENDPOINT}albums/new`).then((res) => {
+      setNewAlbums(res.data);
     });
-    axios.get(`${ENDPOINT}Songs`).then(({ data }) => {
-      setSongs(data);
+    axios.get(`${ENDPOINT}Songs`).then((res) => {
+      setSongs(res.data);
+      setfilteredSongs(res.data);
+    });
+    axios.get(`${ENDPOINT}genres`).then((res) => {
+      setGenre([{ key: "all", label: "All" }, ...res.data.data]);
     });
   }, []);
   return (
@@ -33,7 +39,18 @@ function App() {
       <HeroSection />
       <CardSection tittle="Top Albums" data={topAlbums} />
       <CardSection tittle="New Albums" data={newAlbums} />
-      <FilterSection tittle="Songs" data={Songs} />
+      <FilterSection
+        tittle="Songs"
+        data={filteredSongs}
+        filters={genres}
+        executeFilter={(genre) => {
+          if (genre === "all") {
+            setfilteredSongs(Songs);
+          } else {
+            setfilteredSongs(Songs.filter((song) => song.genre.key === genre));
+          }
+        }}
+      />
     </>
   );
 }
