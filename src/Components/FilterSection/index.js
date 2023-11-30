39f43bcdc9464c625fceddc541cc56a2a19ef93d
Mohammed Muzammil <122904557+mohdmuzzu@users.noreply.github.com>
2023-11-30 21:39:03 +0530

    Add files via upload

--- src/Components/FilterSection/index.js
+++ src/Components/FilterSection/index.js
@@ -1,12 +1,35 @@
 /* eslint-disable import/no-anonymous-default-export */
 import Carousel from "../Carousel";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { useState } from "react";
 
-export default ({ tittle, data }) => {
+import "./FilterSection.css";
+//import { useState } from "react";
+
+export default ({ tittle, data, filters, executeFilter }) => {
+  const [selectedTab, setSelectedTab] = useState(0);
   return (
     <div className="section">
       <div>
         <h1 className="tittle">{tittle}</h1>
       </div>
+      <Tabs
+        value={selectedTab}
+        onChange={(e, value) => {
+          executeFilter(filters[value].key);
+          setSelectedTab(value);
+        }}
+        TabIndicatorProps={{
+          style: {
+            backgroundColor: "green",
+          },
+        }}
+      >
+        {filters.map((f) => (
+          <Tab className="tab" key={f.key} label={f.label} />
+        ))}
+      </Tabs>
       <Carousel data={data} />
     </div>
   );
